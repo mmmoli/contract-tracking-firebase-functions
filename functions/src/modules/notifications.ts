08b7a9326c1835e14f12aae53ca96ebc2ff88ae7
Michele Memoli <michele@100shapes.com>
2020-03-22 00:50:50 +0000

    Twilio config

--- functions/src/modules/notifications.ts
+++ functions/src/modules/notifications.ts
@@ -1,8 +1,8 @@
 import * as functions from 'firebase-functions';
 import twilio from 'twilio';
 
-const accountSid = functions.config().twilio.account_sid;
-const authToken = functions.config().twilio.auth_token;
+const accountSid = functions.config().twilio.account_sid as string;
+const authToken = functions.config().twilio.auth_token as string;
 
 const client = twilio(accountSid, authToken);
 
