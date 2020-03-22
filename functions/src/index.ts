dc217505e6128632fe7bf907abc4b4167c49e3e7
Michele Memoli <michele@100shapes.com>
2020-03-22 00:39:30 +0000

    Added twilio

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -4,6 +4,8 @@ admin.initializeApp(functions.config().firebase);
 
 import * as graph from './modules/graph';
 import * as users from './modules/users';
+import * as notifications from './modules/notifications';
 
 exports.graph = graph;
 exports.users = users;
+exports.notifications = notifications;
--- functions/src/modules/notifications.ts
+++ functions/src/modules/notifications.ts
@@ -1,1 +1,15 @@
-// import * as admin from 'firebase-admin';
+import * as functions from 'firebase-functions';
+import twilio from 'twilio';
+
+const accountSid = functions.config().twilio.account_sid;
+const authToken = functions.config().twilio.auth_token;
+
+const client = twilio(accountSid, authToken);
+
+export const testSMS = functions.https.onCall(() =>
+  client.messages.create({
+    body: `สวัสดีponk`,
+    from: '+12057977380',
+    to: `${functions.config().twilio.test_number}`
+  })
+);
